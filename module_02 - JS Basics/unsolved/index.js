dde4fb82bcace6549515b8523df7d2887a4234db
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-05-14 19:22:10 -0400

    steps 1-3

--- module_02 - JS Basics/unsolved/index.js
+++ module_02 - JS Basics/unsolved/index.js
@@ -1,1 +1,8 @@
 // Your code here
+function greet() {
+    var name = window.prompt("What is your name?");
+    alert("Hello, " + name);
+
+    var age = window.prompt("How old are you?");
+    console.log(parseInt(age));
+}
