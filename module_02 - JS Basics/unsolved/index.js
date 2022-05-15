62dcfc03673fe808f7cf9ad869b68cbfbf640163
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-05-15 13:45:31 -0400

    steps 4-5 in progress

--- module_02 - JS Basics/unsolved/index.js
+++ module_02 - JS Basics/unsolved/index.js
@@ -5,4 +5,24 @@ function greet() {
 
     var age = window.prompt("How old are you?");
     console.log(parseInt(age));
+
+    //Step 4
+    confirm("Have you had a birthday yet this year?");
+
+    /*function confirmAction () {
+        let confirmAction = confirm("Have you had a birthday yet this year?");
+        if (confirmAction) {
+            alert("You were born in ");
+        } else {
+            alert("You were born in ")
+        }
+    }*/
+
+    /*Step 5: use new Date().getFullYear()
+    let today = new Date();
+    let dd = today.getDate();
+    let month = today.getMonth() + 1;
+
+    const yyyy = today.getFullYear(); */
+
 }
